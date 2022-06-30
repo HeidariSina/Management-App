a02844b7b777007d239f80698a3a4e1ec0ba9112
Mahdi <mahdi.ma78@yahoo.com>
2022-06-30 10:48:42 -0700

    merging new changes

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from asyncio import Task
 from importlib.util import set_loader
 import os
 import sys
-from PyQt5 import uic
+from PyQt5 import uic 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication , QMainWindow
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication , QMainWindow , QTableWidgetItem  ,QHeaderView 
 
-Test = uic.loadUiType(os.path.join(os.getcwd() , "QT.ui"))[0]
+Home = uic.loadUiType(os.path.join(os.getcwd() , "QT.ui"))[0]
 Employee = uic.loadUiType(os.path.join(os.getcwd() , "Employee.ui"))[0]
 
 class programData():
@@ -23,10 +22,15 @@ class programData():
         task = Task(name ,startedTime , deadline , importance , milestone  ,persons)
         self.task.append(task)
 
-class Person () :
-    def __init__(self , name ,serialNumber , sex , age , entranceTime , tasks , finishedTasks , unfinishedTasks ) :
+class MileStone() :
+    def __init__(self , name , checked):
         self.name = name
-        self.serialNumber = serialNumber
+        self.checked = checked
+
+class Person () :
+    def __init__(self , name ,ID , sex , age , entranceTime , tasks , finishedTasks , unfinishedTasks ) :
+        self.name = name
+        self.ID = ID
         self.sex = sex
         self.age = age
         self.entranceTime = entranceTime
@@ -44,50 +48,95 @@ class Task () :
         self.persons = persons
 
 
-class MainWindow (QMainWindow , Test):
-    def __init__(self , data):
+class MainWindow (QMainWindow , Home):
+    def __init__(self):
         super(MainWindow , self).__init__()
-        self.data = data
+        self.data = programData()
+        self.addSignal = "none"
         self.setupUi(self)
         self.backButton.hide()
         self.addbutton.hide()
-        self.mainTable.hide()
+        self.tableWidget.hide()
+        self.emptyText.hide()
+        self.emptyText2.hide()
         self.MainText.setStyleSheet("color : white ; border : none")
         self.employeeButton.setStyleSheet("background-color : #1a161c ; color : white")
         self.taskButton.setStyleSheet("background-color : #1a161c ; color : white")
         self.backButton.setStyleSheet("background-color : #1a161c ; color : white")
         self.addbutton.setStyleSheet("background-color : #1a161c ; color : white")
-        self.mainTable.setStyleSheet("background-color : #1a161c ; color : white")
+        self.tableWidget.setStyleSheet("background-color : #1a161c ; color : white")
         self.employeeButton.clicked.connect(self.employ)
         self.taskButton.clicked.connect(self.task)
         self.backButton.clicked.connect(self.back)
-        #self.tableAdd.clicked.connect(self.addRows)
+        self.addbutton.clicked.connect(self.add)
     def employ(self):
+        self.addSignal = "employ"
         self.backButton.show()
         self.addbutton.show()
         self.taskButton.hide()
-        self.mainTable.show()
         self.employeeButton.hide()
         self.MainText.setText("Employees Section")
         self.MainText.setStyleSheet("color : white ; border : none")
         self.MainText.setAlignment(Qt.AlignCenter)
+        if (len(self.data.employ) != 0) :
+            self.tableWidget.show()
+            self.tableWidget.setStyleSheet("background-color : #1a161c ; color : black ; border : none")
+            self.tableWidget.setColumnCount(8)
+            self.tableWidget.setRowCount(8)
+            self.tableWidget.setHorizontalHeaderItem( 0 , QTableWidgetItem("Name"))
+            self.tableWidget.setHorizontalHeaderItem( 1 , QTableWidgetItem("ID"))
+            self.tableWidget.setHorizontalHeaderItem( 2 , QTableWidgetItem("Sex"))
+            self.tableWidget.setHorizontalHeaderItem( 3 , QTableWidgetItem("Age"))
+            self.tableWidget.setHorizontalHeaderItem( 4 , QTableWidgetItem("Entrance Time"))
+            self.tableWidget.setHorizontalHeaderItem( 5 , QTableWidgetItem("Tasks"))
+            self.tableWidget.setHorizontalHeaderItem( 6 , QTableWidgetItem("Finished Tasks"))
+            self.tableWidget.setHorizontalHeaderItem( 7 , QTableWidgetItem("UnFinished Tasks"))
+            self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+            self.tableWidget.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        else :
+            self.emptyText.show()
+            self.emptyText2.show()
+            self.emptyText2.setStyleSheet("color : white ; border : none")
+            self.emptyText.setText("Sorry There is No Employee :(")
+            self.emptyText.setStyleSheet("color : white ; border : none")
+            self.emptyText.setAlignment(Qt.AlignCenter)
     def task(self) :
         self.addSignal = "task"
         self.backButton.show()
         self.addbutton.show()
         self.taskButton.hide()
         self.employeeButton.hide()
-        self.mainTable.show()
         self.MainText.setText("Tasks Section")
         self.MainText.setStyleSheet("color : white; border : none")
         self.MainText.setAlignment(Qt.AlignCenter)
+        if (len(self.data.task) != 0) :
+            self.tableWidget.show()
+            self.tableWidget.setStyleSheet("background-color : #1a161c ; color : black ;border : none")
+            self.tableWidget.setColumnCount(6)
+            self.tableWidget.setRowCount(8)
+            self.tableWidget.setHorizontalHeaderItem( 0 , QTableWidgetItem("Name"))
+            self.tableWidget.setHorizontalHeaderItem( 1 , QTableWidgetItem("Started Time"))
+            self.tableWidget.setHorizontalHeaderItem( 2 , QTableWidgetItem("Deadline"))
+            self.tableWidget.setHorizontalHeaderItem( 3 , QTableWidgetItem("Importance"))
+            self.tableWidget.setHorizontalHeaderItem( 4 , QTableWidgetItem("Milestone"))
+            self.tableWidget.setHorizontalHeaderItem( 5 , QTableWidgetItem("Persons"))
+            self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        else :
+            self.emptyText.show()
+            self.emptyText2.show()
+            self.emptyText2.setStyleSheet("color : white ; border : none")
+            self.emptyText.setText("Sorry There is No Tasks :(")
+            self.emptyText.setStyleSheet("color : white ; border : none")
+            self.emptyText.setAlignment(Qt.AlignCenter)
     def back(self):
         self.addSignal = "none"
+        self.emptyText.hide()
+        self.emptyText2.hide()
         self.addbutton.hide()
         self.backButton.hide()
         self.taskButton.show()
         self.employeeButton.show()
-        self.mainTable.hide()
+        self.tableWidget.hide()
         self.MainText.setText("Main Menu")
         self.MainText.setStyleSheet("color : white; border : none")
         self.MainText.setAlignment(Qt.AlignCenter)
@@ -102,62 +151,20 @@ class MainWindow (QMainWindow , Test):
             return
 
         
-        
-class SecondWindow (QMainWindow , QWidget, Employee):
-    def __init__(self, data):
+class SecondWindow (QMainWindow , Employee):
+    def __init__(self):
         super(SecondWindow, self).__init__()
-        self.data = data
-        self.title = 'Employees List'
-        self.left = 0
-        self.top = 0
-        self.width = 300
-        self.height = 200
-   
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-   
-        self.createTable()
-   
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.tableWidget)
-        self.setLayout(self.layout)
-   
-        #Show window
-        self.show()
-   
-    #Create table
-    def createTable(self):
-        self.tableWidget = QTableWidget()
-  
-        #Row count
-        self.tableWidget.setRowCount(4) 
-  
-        #Column count
-        self.tableWidget.setColumnCount(2)  
-  
-        self.tableWidget.setItem(0,0, QTableWidgetItem("Name"))
-        self.tableWidget.setItem(0,1, QTableWidgetItem("City"))
-        self.tableWidget.setItem(1,0, QTableWidgetItem("Aloysius"))
-        self.tableWidget.setItem(1,1, QTableWidgetItem("Indore"))
-        self.tableWidget.setItem(2,0, QTableWidgetItem("Alan"))
-        self.tableWidget.setItem(2,1, QTableWidgetItem("Bhopal"))
-        self.tableWidget.setItem(3,0, QTableWidgetItem("Arnavi"))
-        self.tableWidget.setItem(3,1, QTableWidgetItem("Mandsaur"))
-   
-        #Table will fit the screen horizontally
-        self.tableWidget.horizontalHeader().setStretchLastSection(True)
-        self.tableWidget.horizontalHeader().setSectionResizeMode(
-            QHeaderView.Stretch)
         self.setupUi(self)
-    #def addRows(self):
-        
-        
+        self.MainText.setStyleSheet("color : white ; border : none")
+        self.NameText.setStyleSheet("color : white ; border : none")
+        self.SerialNumberText.setStyleSheet("color : white ; border : none")
+        self.SexText.setStyleSheet("color : white ; border : none")
+        self.AgeText.setStyleSheet("color : white ; border : none")
+        self.MaleradioButton.setStyleSheet("color : white")
+        self.FemaleradioButton.setStyleSheet("color : white")
 
 if __name__ == "__main__" :
-    data = programData()
     app = QApplication(sys.argv)
-    w = MainWindow(data)
-    #e = SecondWindow(data)
-    #e.show()
+    w = MainWindow()
     w.show()
     sys.exit(app.exec_())
